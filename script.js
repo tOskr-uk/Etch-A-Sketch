e7a3312e984a5edab65eeb663eeb52bda229e4dd
toskr <dccconnect@gmail.com>
2022-09-25 20:31:17 +0100

    js mechanics complete

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const grid = document.querySelector('.grid');
+
+createGrid(64);
+
+grid.addEventListener('mousedown',e=>{
+    paintCell(e.target);
+    if(e.buttons === 1){
+        grid.addEventListener('mouseover',e=>{
+            e.buttons == 1 ? paintCell(e.target) : false;
+        })
+    }
+})
+
+
+function paintCell(cell){
+    if(cell.classList.contains('grid__cell')){
+        cell.style.backgroundColor = 'red';
+    }
+}
+
+function createGrid(size){
+    const gridWidth = grid.offsetWidth;
+    const cellSize = gridWidth/size;
+
+    for(let i=0;i<size*size;i++){
+        const div = document.createElement('div');
+        div.classList.add('grid__cell');
+        div.style.width = `${cellSize}px`
+        div.style.height = `${cellSize}px`
+        grid.appendChild(div);
+    }
+}
